cd2c0bbc03d31da09cb295a9b468324547a15a3f
Jacob <jacob@duijzer.com>
2019-01-28 22:24:51 +0100

    Added first sample integration test

---
+++ src/CleanArchitectureTemplate.IntegrationTests/Api/ValuesControllerShould.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchitectureTemplate.IntegrationTests.Helpers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanArchitectureTemplate.IntegrationTests.Api
+{
+    public class ValuesControllerShould : IClassFixture<ApiTestFixture>
+    {
+        private readonly ApiTestFixture _fixture;
+
+        public ValuesControllerShould(ApiTestFixture fixture) =>
+            _fixture = fixture;
+
+        [Fact]
+        public async Task ReturnOk()
+        {
+            var result = await _fixture.HttpClient.GetAsync("/api/values").ConfigureAwait(false);
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ src/CleanArchitectureTemplate.IntegrationTests/Helpers/ApiTestFixture.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+
+namespace CleanArchitectureTemplate.IntegrationTests.Helpers
+{
+    public class ApiTestFixture : IDisposable
+    {
+        public readonly HttpClient HttpClient;
+
+        public ApiTestFixture()
+        {
+            var server = new TestServer(new WebHostBuilder()
+                .UseEnvironment(EnvironmentName.Development)
+                .ConfigureTestServices((IServiceCollection serviceCollection) =>
+                {
+                    
+                })
+                .UseStartup<CleanArchitectureTemplate.Api.Startup>());
+
+           HttpClient  = server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
