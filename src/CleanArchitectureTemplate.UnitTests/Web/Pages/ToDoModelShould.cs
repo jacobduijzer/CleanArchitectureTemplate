54c63c49aeb248e47402fe33a6a3be51ea72c080
Jacob Duijzer <jduijzer@vaa.com>
2019-02-07 12:25:56 +0100

    Added Web razor viewmodel unit test

---
+++ src/CleanArchitectureTemplate.UnitTests/Web/Pages/ToDoModelShould.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitectureTemplate.Application.ToDoItems.UseCases;
+using CleanArchitectureTemplate.Web.Pages;
+using MediatR;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CleanArchitectureTemplate.UnitTests.Web.Pages
+{
+    public class ToDoModelShould
+    {
+        [Fact]
+        public async Task CallAsyncHandler()
+        {
+            var mockMediator = new Mock<IMediator>();
+
+            var viewModel = new ToDoModel(mockMediator.Object);
+            await viewModel.OnGetAsync();
+
+            mockMediator.Verify(x => x.Send(It.IsAny<ToDoItemsRequest>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
