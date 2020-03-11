66f844bd4a8fcd6f65dc54e2fb54aedaac025fbb
Jacob Duijzer <jacob@duijzer.com>
2020-03-11 10:41:58 +0100

    WIP: test-projects script

--- test-projects.sh
+++ test-projects.sh
@@ -1,11 +1,45 @@
 #!/bin/bash
 
-set -x
+function GetNugetPackage
+{
+    echo "GetNugetPackage"
+    pattern="JacobsApps.CleanArchitectureProjectTemplate.CSharp.*.nupkg"
+    files=( $pattern )
+    nugetFile="${files[0]}"
+    echo "Package found: " $nugetFile 
+}
 
-dotnet new -i /home/vsts/work/1/s/JacobsApps.CleanArchitectureProjectTemplate.CSharp.2.5.0.nupkg
+function InstallNugetPackage
+{
+    echo "InstallNugetPackage"
+    dotnet new -i $nugetFile
+}
 
-mkdir FullWeb 
-cd FullWeb 
-dotnet new cleanarchitecture -uc fullweb
-dotnet build
-dotnet test
+function CreateBuildAndTestProject
+{
+    echo "Creating test project: " $1
+
+    useCase=${1,,}
+
+    mkdir $1
+    cd $1
+    dotnet new cleanarchitecture -uc $useCase
+    dotnet build
+    dotnet test
+}
+
+GetNugetPackage
+
+InstallNugetPackage
+
+CreateBuildAndTestProject Full
+# CreateBuildAndTestProject FullWeb
+
+# echo $nugetFile
+# dotnet new -i /home/vsts/work/1/s/JacobsApps.CleanArchitectureProjectTemplate.CSharp.2.5.0.nupkg
+
+# mkdir FullWeb 
+# cd FullWeb 
+# dotnet new cleanarchitecture -uc fullweb
+# dotnet build
+# dotnet test
