d522f81848c7ba3ad220e9d8ae5d6126bca8155d
Jacob Duijzer <jacob@duijzer.com>
2020-03-11 16:18:45 +0100

    Test scripts with pipeline

--- test-nuget-package.sh
+++ test-nuget-package.sh
@@ -32,14 +32,5 @@ GetNugetPackage
 
 InstallNugetPackage
 
-CreateBuildAndTestProject EmptyApi 
+CreateBuildAndTestProject $1 
 #CreateBuildAndTestProject FullWeb
-
-# echo $nugetFile
-# dotnet new -i /home/vsts/work/1/s/JacobsApps.CleanArchitectureProjectTemplate.CSharp.2.5.0.nupkg
-
-# mkdir FullWeb 
-# cd FullWeb 
-# dotnet new cleanarchitecture -uc fullweb
-# dotnet build
-# dotnet test
